e8a95379289e013b7336ec97052e4256ac5e6f62
geniuus <cross_man@naver.com>
2023-08-16 16:26:10 +0900

    캘린더 주간 월간 UI 전환 완료
    collectionView delegate, datasource 잡아야함

--- Exercisary/DetailCollectionViewCell.swift
+++ Exercisary/DetailCollectionViewCell.swift
@@ -9,18 +9,10 @@ import UIKit
 
 class DetailCollectionViewCell: UICollectionViewCell {
     
-    @IBOutlet weak var markerView: UIView!
-    @IBOutlet weak var imageView: UIImageView!
-    @IBOutlet weak var timeLabel: UILabel!
     @IBOutlet weak var kindLabel: UILabel!
     @IBOutlet weak var memoLabel: UILabel!
+    @IBOutlet weak var contentLabel: UILabel!
+    @IBOutlet weak var timeLabel: UILabel!
     
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        memoLabel.layer.borderColor = UIColor.lightGray.cgColor
-        memoLabel.layer.borderWidth = 0.7
-        memoLabel.layer.cornerRadius = 20
-        
-        markerView.layer.cornerRadius = 5
-    }
+    
 }
